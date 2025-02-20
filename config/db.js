f4cd2d5f141970db2d0bc2dbcb5e3a8b10349205
AayushJain27 <ayush@serviceplug.in>
2025-02-20 20:05:39 +0530

    fixed issue

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 // const mongoURI = "mongodb://localhost:27017";
 const mongoURI = "mongodb+srv://unifitz:Unifitz@123@unifitz.vipt0.mongodb.net/?retryWrites=true&w=majority&appName=Unifitz";
+// const mongoURI = "mongodb+srv://unifitz:Unifitz%40123@unifitz.vipt0.mongodb.net/?retryWrites=true&w=majority&appName=Unifitz";
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
       })
         .then(() => console.log('Connected to MongoDB'))
         .catch((error) => console.error('Error connecting to MongoDB:', error));
