6081b48c7d329bcc677a1de95055c8e090b017fe
AayushJain27 <ayush@serviceplug.in>
2025-02-20 20:07:34 +0530

    fixed isuse

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 // const mongoURI = "mongodb://localhost:27017";
-const mongoURI = "mongodb+srv://unifitz:Unifitz@123@unifitz.vipt0.mongodb.net/?retryWrites=true&w=majority&appName=Unifitz";
-// const mongoURI = "mongodb+srv://unifitz:Unifitz%40123@unifitz.vipt0.mongodb.net/?retryWrites=true&w=majority&appName=Unifitz";
+// const mongoURI = "mongodb+srv://unifitz:Unifitz@123@unifitz.vipt0.mongodb.net/?retryWrites=true&w=majority&appName=Unifitz";
+const mongoURI = "mongodb+srv://unifitz:Unifitz%40123@unifitz.vipt0.mongodb.net/?retryWrites=true&w=majority&appName=Unifitz";
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI, {
