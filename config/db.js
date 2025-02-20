263ebb52eb45c8d548ee71fb5648029a35fb98a2
AayushJain27 <ayush@serviceplug.in>
2025-02-20 19:52:34 +0530

    change database url

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-const mongoURI = "mongodb://localhost:27017";
+// const mongoURI = "mongodb://localhost:27017";
+const mongoURI = "mongodb+srv://unifitz:Unifitz@123@unifitz.vipt0.mongodb.net/?retryWrites=true&w=majority&appName=Unifitz";
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI, {
